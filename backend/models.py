6a3c6b649a49a01aa8c4bda32239a62db17cfd8e
Jo Lily <lily@example.com>
2025-08-03 21:29:12 +0900

    ユーザー関連のクラス定義を追加

---
+++ backend/models.py
@@ -0,0 +1,39 @@
+from datetime import date, datetime
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+# ユーザー新規作成リクエスト
+class User_create(BaseModel):
+    name: str
+    email: str
+    password: str
+    birth_date: Optional[date] = None
+    # バリデーター
+    @field_validator("name")
+    def validate_name(cls, v):
+        if(len(v) < 3):
+            raise ValueError("ユーザー名は3文字以上で入力してください")
+        return v
+    @field_validator("password")
+    def validate_password(cls, v):
+        if(len(v) < 6):
+            raise ValueError("パスワードは6文字以上で入力してください")
+        return v
+    
+# ユーザー情報
+class User_response(BaseModel):
+    id: int
+    username: str
+    email: str
+    birthDate: Optional[date] = None
+    createdAt: datetime
+    
+# ユーザー新規作成レスポンス
+class User_create_Response(BaseModel):
+    user: User_response
+    token: str
+    
+# トークン
+class Token(BaseModel):
+    token: str
+    token_typp: str = "bearer"
