873fefdfffb0474e1f0632996a752c7f851ce221
Jo Lily <lily@example.com>
2025-08-03 21:27:43 +0900

    DB処理を追加

---
+++ backend/infrastructure/db.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+# .envファイルの読み込み
+load_dotenv()
+
+# DB接続文字列
+DB_URL = os.getenv("DATABASE_URL")
+
+#　基底クラス
+class Base(DeclarativeBase):
+    pass
+
+# DBエンジン定義
+engine = create_engine(DB_URL)
+
+# テーブル作成（初回のみ）
+def create_tables():
+    Base.metadata.create_all(engine)
+# DBセッション作成・取得
+def session():
+    create_tables()
+    SessionClass = sessionmaker(engine)
+    session = SessionClass()
+    return session
