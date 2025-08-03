986b21f597a555c64ae4e856ec9ff2d92260bec0
Jo Lily <lily@example.com>
2025-08-03 22:57:17 +0900

    JWT関連処理を追加

---
+++ backend/auth/jwt.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta, timezone
+from pydantic import BaseModel
+from typing import Union
+from dotenv import load_dotenv
+import os
+import jwt
+
+# 環境変数ファイル読み込み
+load_dotenv()
+
+# 署名作成に使用する秘密鍵
+# 下記コマンドで作成:
+# $ openssl rand -hex 32
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM")
+
+# 日本標準時タイムゾーン(+9h)
+JST = timezone(timedelta(hours=9))
+
+class Token(BaseModel):
+    token: str
+
+# JWTトークン生成
+def create_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc).astimezone(JST) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc).astimezone(JST) + timedelta(minutes=60)
+    # 有効期限を更新
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
