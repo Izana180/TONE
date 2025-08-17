7e04f21f3f35804c83a9efe15238ada7c7ac25a4
Jo Lily <lily@example.com>
2025-08-17 12:49:14 +0900

    JWT処理用環境変数を追加

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,9 @@ env_file = os.getenv("ENV_FILE", ".env.dev")
 load_dotenv(env_file)
 
 database_url = os.getenv("DATABASE_URL")
+# JWTトークン署名作成に使用する秘密鍵
+# 下記コマンドで作成:
+# $ openssl rand -hex 32
+jwt_secret_key = os.getenv("JWT_SECRET_KEY")
+# JWTトークン署名作成時のアルゴリズム
+jwt_algorithm = os.getenv("JWT_ALGORITHM")
