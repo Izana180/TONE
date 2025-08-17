55285fef1777d9e8ede87c23746ec0749fd7378d
Jo Lily <lily@example.com>
2025-08-17 12:46:00 +0900

    実装：JWT処理-環境変数取得先を変更

--- backend/auth/jwt.py
+++ backend/auth/jwt.py
@@ -1,17 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from typing import Union
-from dotenv import load_dotenv
-import os
+from config import jwt_secret_key, jwt_algorithm
 import jwt
 
-# 環境変数ファイル読み込み
-load_dotenv()
-# 署名作成に使用する秘密鍵
-# 下記コマンドで作成:
-# $ openssl rand -hex 32
-SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-# 署名作成時のアルゴリズム
-ALGORITHM = os.getenv("JWT_ALGORITHM")
 # 日本標準時タイムゾーン(+9h)
 JST = timezone(timedelta(hours=9))
 # JWTトークン生成
@@ -23,5 +14,5 @@ def create_token(data: dict, expires_delta: Union[timedelta, None] = None) -> st
         expire = datetime.now(timezone.utc).astimezone(JST) + timedelta(minutes=60)
     # 有効期限を更新
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, jwt_secret_key, algorithm=jwt_algorithm)
     return encoded_jwt
