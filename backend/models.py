ab840cfde4a792c8c06b5080d695105ad32450e0
Jo Lily <lily@example.com>
2025-08-25 04:32:16 +0900

    指摘対応：パスワードバリデーション

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from pydantic import BaseModel, field_validator
 from typing import Optional
+import re
 
 # ユーザー新規作成リクエスト
 class UserCreate(BaseModel):
@@ -13,12 +14,21 @@ class UserCreate(BaseModel):
     def validate_name(cls, v):
         if(len(v) < 3):
             raise ValueError("ユーザー名は3文字以上で入力してください")
+        
         return v
     @field_validator("password")
-    def validate_password(cls, v):
-        if(len(v) < 6):
-            raise ValueError("パスワードは6文字以上で入力してください")
-        return v
+    def validate_password(cls, value):
+        if(len(value) < 10):
+            raise ValueError("パスワードは10文字以上で入力してください")
+        elif not re.search(r'[A-Z]', value):
+            raise ValueError("パスワードには英大文字を含めてください")
+        elif not re.search(r'[a-z]', value):
+            raise ValueError("パスワードには英小文字を含めてください")
+        elif not re.search(r'\d', value):
+            raise ValueError("パスワードには数字を含めてください")
+        elif not re.search(r'[!@#$%^&*()_+\-=\[\]{};\'\\:"|,<.>/?]', value):
+            raise ValueError("パスワードには記号を1つ以上含めてください")
+        return value
     
 # ユーザー情報
 class UserResponse(BaseModel):
